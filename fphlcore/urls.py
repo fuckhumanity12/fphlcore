917fce118afdc257baaede8abc0a06db13b849f1
Mohab Gabber <mohabgabber1@protonmail.com>
2022-11-07 16:14:09 +0200

    added favicon

--- fphlcore/urls.py
+++ fphlcore/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("articles.urls")),
     path("auth/", include("users.urls")),
+    path("favicon.ico", RedirectView.as_view(
+        url=staticfiles_storage.url("favicon.ico")),),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
